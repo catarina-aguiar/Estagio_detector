d5c97280bb19ed99411bd895e753e3670ee6a2d1
catarina-aguiar <167872707+catarina-aguiar@users.noreply.github.com>
2026-06-01 14:28:42 +0100

    changes to the llm used

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def analyze():
 
     try:
         response = client.chat.completions.create(
-            model="meta-llama/llama-4-maverick-17b-128e-instruct",
+            model="model="meta-llama/llama-4-scout-17b-16e-instruct",
             messages=[
                 {"role": "system", "content": SYSTEM_PROMPT},
                 {"role": "user", "content": f"Analisa esta questão: {question}"}
