36d65787cac4035c75039a3827a7ab1953604d52
catarina-aguiar <167872707+catarina-aguiar@users.noreply.github.com>
2026-06-09 21:18:55 +0100

    back to detector

---
+++ prompt_questaogeral.py
@@ -0,0 +1,3 @@
+SYSTEM_PROMPT = """
+For each of the following questions, select the correct answer. Answer each question independently, without considering any previous questions or context. Each question has only one correct answer
+"""
