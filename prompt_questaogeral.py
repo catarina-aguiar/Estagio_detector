3e3ffb9aca50df3f443f5eb49ad476d566b88c83
catarina-aguiar <167872707+catarina-aguiar@users.noreply.github.com>
2026-06-15 13:21:37 +0100

    use of normal llama

--- prompt_questaogeral.py
+++
@@ -1,3 +0,0 @@
-SYSTEM_PROMPT = """
-For each of the following questions, select the correct answer. Answer each question independently, without considering any previous questions or context. Each question has only one correct answer
-"""
